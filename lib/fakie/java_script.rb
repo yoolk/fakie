6ebe950117e728a9c0a1fcfc6c7c47570f57c439
Sam Soffes <sam@soff.es>
2013-03-12 16:53:38 -0400

    Fix JavaScript path

--- lib/fakie/java_script.rb
+++ lib/fakie/java_script.rb
@@ -7,8 +7,9 @@ module Fakie
     def js_context
       @@_js_context ||= begin
         require 'execjs'
-        source = File.open(File.join(__FILE__, '../js/libphonenumber.js')).read
-        source += File.open(File.join(__FILE__, '../js/fakie.js')).read
+        js_dir = File.join(File.expand_path(File.dirname(__FILE__)), 'js')
+        source = File.open(File.join(js_dir, 'libphonenumber.js')).read
+        source += File.open(File.join(js_dir, 'fakie.js')).read
         ExecJS.compile(source)
       end
     end
